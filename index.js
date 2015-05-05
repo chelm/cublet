8d5187e833c919b7ed6e7cd4e0f2518a848b5d99
chelm <christopher.helm@gmail.com>
2015-05-05 08:59:59 -0600

    support for torque style tiles

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ var stream = require("JSONStream"),
   mkdirp = require('mkdirp')
 
 var argv = require('optimist')
-  .usage('Usage: $0 -l MinZoom,MaxZoom -a Attribute -t TimeAttribute')
+  .usage('Usage: $0 -l MinZoom,MaxZoom -a Attribute -t TimeAttribute -s {torque|time}')
   .default('l', '0,6')
   .default('a', null)
   .default('t', null)
+  .default('s', 'time')
   .argv
 
 var levels = argv.l.split(',')
@@ -19,7 +20,8 @@ var agg = {},
 
 var q = async.queue(function (t, cb) {
   mkdirp(t.path, function (err) {
-    var pxData = {}
+    //var pxData = {}
+    var pxData = []
 
     for (var px in t.tile) {
       var xy = px.split('-')
@@ -27,20 +29,30 @@ var q = async.queue(function (t, cb) {
       var values = []
 
       for (var time in t.tile[px].values){
-        var date = new Date(time)
-        if (date.getTime() < minTime || !minTime){
-          minTime = date
+        var epoch = new Date(time).getTime()
+        var day = ~~((epoch-minTime)/(1000*60*60*24))
+        // Torque style tiles 
+        times.push(day)
+        values.push(t.tile[px].values[time])
+
+        // Time based tiles
+        if (argv.s === 'time'){ 
+          if (!pxData[day]){
+            pxData[day] = []
+          }
+          pxData[day].push({
+            x: xy[0],
+            y: xy[1],
+            v: t.tile[px].values[time]
+          })
         }
-        if (date.getTime() > maxTime || !maxTime){
-          maxTime = date
-        }
-        if (!pxData[date]){
-          pxData[date] = []
-        }
-        pxData[date].push({
+      }
+      if (argv.s === 'torque'){
+        pxData.push({
           x: xy[0],
           y: xy[1],
-          v: t.tile[px].values[time]
+          t: times,
+          v: values
         })
       }
     }
@@ -52,6 +64,7 @@ var q = async.queue(function (t, cb) {
 q.drain = function(){
   console.log('Min Time:', minTime)
   console.log('Max Time:', maxTime);
+  console.log(((maxTime-minTime)/(1000*60*60*24))/365, 'Total days')
 }
 
 var parser = stream.parse('features.*');
@@ -73,6 +86,10 @@ process.stdin
     var coords = data.geometry.coordinates,
       value = data.properties[argv.a],
       time = data.properties[argv.t]
+
+    var date = new Date(time)    
+    minTime = (!minTime) ? date : Math.min(date, minTime)
+    maxTime = (!maxTime) ? date : Math.max(date, maxTime)
 
     tilePixel(coords[0], coords[1], levels[0], parseInt(levels[1])+1, function (err, pxAgg) {
       for (var z in pxAgg){
